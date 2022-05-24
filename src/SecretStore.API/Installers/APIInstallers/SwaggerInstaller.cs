4932e91ad0ca9b2516da0ee56d883ec7081acd3a
Ravinder Chahal <chahaltechie@gmail.com>
2022-05-24 20:39:42 +0530

    Created the separate IInstaller implementation for Swagger configuration in servicecollection pipeline.

---
+++ src/SecretStore.API/Installers/APIInstallers/SwaggerInstaller.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace SecretStore.API.Installers.APIInstallers
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation  
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+    }
+}
--- src/SecretStore.API/Startup.cs
+++ src/SecretStore.API/Startup.cs
@@ -1,24 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediatR;
-using SecretStore.API.Configurations;
 using SecretStore.API.Installers;
-using SecretStore.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 namespace SecretStore.API
@@ -37,56 +22,6 @@ namespace SecretStore.API
         {
             services.AddHttpContextAccessor();
             services.InstallServicesInAssembly(Configuration);
-            // services
-            //     .AddAuthentication(x =>
-            //     {
-            //         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //         x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            //         x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //     })
-            //     .AddJwtBearer(x =>
-            //     {
-            //         x.SaveToken = true;
-            //         x.TokenValidationParameters = new TokenValidationParameters
-            //         {
-            //             ValidateIssuerSigningKey = true,
-            //             IssuerSigningKey =
-            //                 new SymmetricSecurityKey(Encoding.ASCII.GetBytes("ThisIsASecretKeyWhichNoOneCanHack")),
-            //             ValidateIssuer = false,
-            //             ValidateAudience = false,
-            //             RequireExpirationTime = false,
-            //             ValidateLifetime = true
-            //         };
-            //     }); 
-            services.AddSwaggerGen(swagger =>
-            {
-                //This is to generate the Default UI of Swagger Documentation  
-                // To Enable authorization using Swagger (JWT)  
-                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description =
-                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-                });
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] { }
-                    }
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
